ff6eca34b67fd175f9fe199e3f6b5b91c1e654fa
FAshkani <Fred.Ashkani@gmail.com>
2026-02-16 18:06:24 +1100

    Update territories.sql

--- models/silver/territories.sql
+++ models/silver/territories.sql
@@ -8,7 +8,28 @@
 
 -- CTE to rank CDC records by Id, meta_ExtractedDate, and SYS_CHANGE_VERSION
 WITH source_data AS (
-    select * FROM {{ source('bronze','territories') }} t1
+select
+  TerritoryID,
+  Name,
+  CountryRegionCode,
+  "Group",
+  SalesYTD,
+  SalesLastYear,
+  CostYTD,
+  CostLastYear,
+  rowguid,
+  ModifiedDate
+from {{ source('bronze','territories') }} t1
 )
-
-select * from source_data
+select
+  TerritoryID,
+  Name,
+  CountryRegionCode,
+  "Group",
+  SalesYTD,
+  SalesLastYear,
+  CostYTD,
+  CostLastYear,
+  rowguid,
+  ModifiedDate
+from   source_data
