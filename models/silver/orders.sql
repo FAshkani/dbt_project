5c764f4b4ee82e107823d8908a9e5e3115fb6937
FAshkani <Fred.Ashkani@gmail.com>
2026-02-16 18:05:53 +1100

    Update orders.sql

--- models/silver/orders.sql
+++ models/silver/orders.sql
@@ -8,7 +8,62 @@
 
 -- CTE to rank CDC records by Id, meta_ExtractedDate, and SYS_CHANGE_VERSION
 WITH source_data AS (
-    select * FROM {{ source('bronze','orders') }} t1
+select
+  SalesOrderID,
+  RevisionNumber,
+  OrderDate,
+  DueDate,
+  ShipDate,
+  Status,
+  OnlineOrderFlag,
+  SalesOrderNumber,
+  PurchaseOrderNumber,
+  AccountNumber,
+  CustomerID,
+  SalesPersonID,
+  TerritoryID,
+  BillToAddressID,
+  ShipToAddressID,
+  ShipMethodID,
+  CreditCardID,
+  CreditCardApprovalCode,
+  CurrencyRateID,
+  SubTotal,
+  TaxAmt,
+  Freight,
+  TotalDue,
+  Comment,
+  rowguid,
+  ModifiedDate
+from {{ source('bronze','orders') }} t1
 )
 
-select * from source_data
+select
+  SalesOrderID,
+  RevisionNumber,
+  OrderDate,
+  DueDate,
+  ShipDate,
+  Status,
+  OnlineOrderFlag,
+  SalesOrderNumber,
+  PurchaseOrderNumber,
+  AccountNumber,
+  CustomerID,
+  SalesPersonID,
+  TerritoryID,
+  BillToAddressID,
+  ShipToAddressID,
+  ShipMethodID,
+  CreditCardID,
+  CreditCardApprovalCode,
+  CurrencyRateID,
+  SubTotal,
+  TaxAmt,
+  Freight,
+  TotalDue,
+  Comment,
+  rowguid,
+  ModifiedDate
+ 
+from  source_data
