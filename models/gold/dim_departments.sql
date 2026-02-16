fa0659353c5025c03dc0a21c527fbdcc924cd8be
FAshkani <Fred.Ashkani@gmail.com>
2026-02-16 18:08:11 +1100

    Refactor SQL to select specific columns from source_data

--- models/gold/dim_departments.sql
+++ models/gold/dim_departments.sql
@@ -8,7 +8,17 @@
 
 -- CTE to rank CDC records by Id, meta_ExtractedDate, and SYS_CHANGE_VERSION
 WITH source_data AS (
-    select * FROM {{ ref('departments') }} t1
+select
+  DepartmentID,
+  Name,
+  GroupName,
+  ModifiedDate
+from {{ ref('departments') }} t1
 )
 
-select * from source_data
+select
+  DepartmentID,
+  Name,
+  GroupName,
+  ModifiedDate
+from  source_data
