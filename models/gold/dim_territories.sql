9b7500b6e33b4b079a79c6c1fa8611540454b5b9
FAshkani <Fred.Ashkani@gmail.com>
2026-02-16 18:09:04 +1100

    Update SQL to select specific columns from territories
    Refactored SQL to select specific columns from territories and source_data.

--- models/gold/dim_territories.sql
+++ models/gold/dim_territories.sql
@@ -8,7 +8,29 @@
 
 -- CTE to rank CDC records by Id, meta_ExtractedDate, and SYS_CHANGE_VERSION
 WITH source_data AS (
-    select * FROM {{ ref('territories') }} t1
+select
+  TerritoryID,
+  Name,
+  CountryRegionCode,
+  "Group",
+  SalesYTD,
+  SalesLastYear,
+  CostYTD,
+  CostLastYear,
+  rowguid,
+  ModifiedDate
+from {{ ref('territories') }} t1
 )
 
-select * from source_data
+select
+  TerritoryID,
+  Name,
+  CountryRegionCode,
+  "Group",
+  SalesYTD,
+  SalesLastYear,
+  CostYTD,
+  CostLastYear,
+  rowguid,
+  ModifiedDate
+from  source_data
