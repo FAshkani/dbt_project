30bef80cb97bb675682d3905ab02bcb7bbf0d821
FAshkani <Fred.Ashkani@gmail.com>
2026-02-16 18:08:30 +1100

    Update dim_products.sql

--- models/gold/dim_products.sql
+++ models/gold/dim_products.sql
@@ -8,7 +8,59 @@
 
 -- CTE to rank CDC records by Id, meta_ExtractedDate, and SYS_CHANGE_VERSION
 WITH source_data AS (
-    select * FROM {{ ref('products') }} t1
+select
+  ProductID,
+  Name,
+  ProductNumber,
+  MakeFlag,
+  FinishedGoodsFlag,
+  Color,
+  SafetyStockLevel,
+  ReorderPoint,
+  StandardCost,
+  ListPrice,
+  Size,
+  SizeUnitMeasureCode,
+  WeightUnitMeasureCode,
+  Weight,
+  DaysToManufacture,
+  ProductLine,
+  Class,
+  Style,
+  ProductSubcategoryID,
+  ProductModelID,
+  SellStartDate,
+  SellEndDate,
+  DiscontinuedDate,
+  rowguid,
+  ModifiedDate
+from {{ ref('products') }} t1
 )
 
-select * from source_data
+select
+  ProductID,
+  Name,
+  ProductNumber,
+  MakeFlag,
+  FinishedGoodsFlag,
+  Color,
+  SafetyStockLevel,
+  ReorderPoint,
+  StandardCost,
+  ListPrice,
+  Size,
+  SizeUnitMeasureCode,
+  WeightUnitMeasureCode,
+  Weight,
+  DaysToManufacture,
+  ProductLine,
+  Class,
+  Style,
+  ProductSubcategoryID,
+  ProductModelID,
+  SellStartDate,
+  SellEndDate,
+  DiscontinuedDate,
+  rowguid,
+  ModifiedDate
+from  source_data
